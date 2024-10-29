d37559da87773b8fb07f8aafbe65c3757c7e8b72
Pablo <pmacsal2707@g.educaand.es>
2024-10-29 13:03:04 +0100

    codigo

---
+++ git_auto.bat
@@ -0,0 +1,8 @@
+@echo off
+:: Ejecuta los comandos git - 
+git add .
+git commit -m "codigo"
+git push origin master
+
+echo Cambios agregados, commit realizado y push ejecutado en la rama master.
+:: pause - esto hace que tengamos que darle a una tecla para continuar
